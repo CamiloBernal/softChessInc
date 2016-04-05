2fe5d2ecc19ac8c09196fe734ffe0243956f3c50
camilobernal <angelcamilob@gmail.com>
2016-04-04 20:34:06 -0500

    Create web api

---
+++ src/app/SoftChess.Inc.Api.Web/Controllers/HistoricalController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using SoftChess.Inc.Core.DataContracts;
+using SoftChess.Inc.Data.DataProxies;
+
+namespace SoftChess.Inc.Api.Web.Controllers
+{
+    public class HistoricalController : ApiController
+    {
+        private readonly IHistoricalPersistence _historicalPersistence;
+
+        public HistoricalController(IHistoricalPersistence historicalPersistence)
+        {
+            if (historicalPersistence == null) throw new ArgumentNullException(nameof(historicalPersistence));
+            _historicalPersistence = historicalPersistence;
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] HistoricalMovement movement)
+        {
+            if (movement == null) throw new ArgumentNullException(nameof(movement));
+            await _historicalPersistence.RegisterPieceMovementAsync(movement).ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/app/SoftChess.Inc.Api.Web/Controllers/RulesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using SoftChess.Inc.Core.DataContracts;
+using SoftChess.Inc.Data.DataProxies;
+
+namespace SoftChess.Inc.Api.Web.Controllers
+{
+    public class RulesController : ApiController
+    {
+        private readonly IRulePersistence _ruleDbContext;
+
+        public RulesController(IRulePersistence ruleDbContext)
+        {
+            if (ruleDbContext == null) throw new ArgumentNullException(nameof(ruleDbContext));
+            _ruleDbContext = ruleDbContext;
+        }
+
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get([FromBody] RuleValidationRequest validationRequest)
+        {
+            var pieceRuleSet =
+                await _ruleDbContext.GetPieceRulesetAsync(validationRequest.PieceType).ConfigureAwait(false);
+            var validationResult = pieceRuleSet.MovementIsValid(validationRequest.CurrentPosition,
+                validationRequest.NextPosition);
+            if (validationResult.Item1)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new RuleValidationResponse
+                {
+                    Message = "Ok"
+                });
+            }
+            var forbidenResponse = new RuleValidationResponse
+            {
+                Message = validationResult.Item2
+            };
+            forbidenResponse.AvailableMovements.AddRange(
+                pieceRuleSet.GetAvailableMovements(validationRequest.CurrentPosition));
+
+            return Request.CreateResponse(HttpStatusCode.Forbidden, forbidenResponse);
+        }
+    }
+}
